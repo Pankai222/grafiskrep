7d1dbfb8a315986045dd58827c3a4324d10d0ee3
Primdahl26 <Kyz74ntu>
2019-12-05 18:21:05 +0100

    Tilføjet grafiskrep logo - mangler at få transparant baggrund
    Tilføjet basic controller til alle sider
    
    lavet om på index og contact html siden - gjordt så headeren skifter side

--- src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/CatalogueController.java
+++ src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/CatalogueController.java
@@ -1,4 +1,13 @@
 package dat19v2.projektgrafiskrep.grafiskrep.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class CatalogueController {
+
+    @RequestMapping("/catalogue")
+    public String catalogue(){
+        return "catalogue";
+    }
 }
