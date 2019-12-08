23a321dd1162117e5ab24773066ce33f895cc4a5
Niklas Lund Brock <niklasbrock@gmail.com>
2019-12-08 17:17:02 +0100

    Started working on CatalogueController Mostly committing to check for pulls

--- src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/CatalogueController.java
+++ src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/CatalogueController.java
@@ -1,13 +1,23 @@
 package dat19v2.projektgrafiskrep.grafiskrep.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class CatalogueController {
 
     @RequestMapping("/catalogue")
-    public String catalogue(){
+    public String catalogue(Model model, HttpSession httpSession){
         return "catalogue";
     }
+
+    @PostMapping ("add-to-cart")
+    public String addToCart(Model model, HttpSession httpSession){
+        return null;
+    }
 }
+
