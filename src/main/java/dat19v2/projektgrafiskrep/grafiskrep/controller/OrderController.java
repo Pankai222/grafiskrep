f3fb034b768ca8b5ab4fba5195f29d1703dbd918
frederikprimdahl <frederik.vincent@hotmail.com>
2019-12-10 13:31:29 +0100

    Tilføjet funktionalitet til ordercontroller og order HTML siden

--- src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/OrderController.java
+++ src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/OrderController.java
@@ -1,12 +1,29 @@
 package dat19v2.projektgrafiskrep.grafiskrep.controller;
 
+import dat19v2.projektgrafiskrep.grafiskrep.databaseservice.MachineDAO;
+import dat19v2.projektgrafiskrep.grafiskrep.model.Machine;
+import dat19v2.projektgrafiskrep.grafiskrep.model.pos.Sale;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class OrderController {
+    @ModelAttribute("items")
+    List<Machine> items(){
+
+        MachineDAO machineDAO = new MachineDAO();
+        List<Machine> machines = machineDAO.selectAll();
+
+        return machines;
+    }
+
     @RequestMapping("/order")
-    public String order(){
+    public String order() {
+
         return "order";
     }
 }
