2f9432a35a974395702e87e24bd82e0b518b4e21
Niklas Lund Brock <niklasbrock@gmail.com>
2019-12-12 00:18:12 +0100

    Made "Din Kurv" work with all the added items in checkout page.

--- src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/CheckoutController.java
+++ src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/CheckoutController.java
@@ -1,5 +1,6 @@
 package dat19v2.projektgrafiskrep.grafiskrep.controller;
 
+import dat19v2.projektgrafiskrep.grafiskrep.model.MachinePart;
 import dat19v2.projektgrafiskrep.grafiskrep.model.pos.Sale;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,12 +8,22 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 @Controller
 public class CheckoutController {
+
+    @ModelAttribute("saleitems")
+    public ArrayList<MachinePart> saleitems (HttpSession httpSession){
+        Sale sale = (Sale) httpSession.getAttribute("sale");
+        return sale.getItems();
+
+    }
+
     @RequestMapping("/checkout")
-    public String checkout(@ModelAttribute("sale") Sale sale, HttpSession httpSession){
-        sale = (Sale) httpSession.getAttribute("sale");
+    public String checkout(Model model, HttpSession httpSession){
+        Sale sale = (Sale) httpSession.getAttribute("sale");
+        model.addAttribute("sale", sale);
         return "checkout";
     }
 }
