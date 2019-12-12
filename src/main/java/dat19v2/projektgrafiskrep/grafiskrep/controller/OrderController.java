9684dae52e1d77f2f8177085044175faa6ac5d9f
nullxDEADBEEF <andxdgu@gmail.com>
2019-12-12 07:10:10 +0100

    Added phone number row in order, serviceContractOrder is still work in progress, almost there

--- src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/OrderController.java
+++ src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/OrderController.java
@@ -1,31 +1,27 @@
 package dat19v2.projektgrafiskrep.grafiskrep.controller;
 
+import dat19v2.projektgrafiskrep.grafiskrep.databaseservice.CustomerDAO;
 import dat19v2.projektgrafiskrep.grafiskrep.databaseservice.MachineDAO;
+import dat19v2.projektgrafiskrep.grafiskrep.databaseservice.ServiceContractDAO;
+import dat19v2.projektgrafiskrep.grafiskrep.databaseservice.ServiceContractOrderDAO;
+import dat19v2.projektgrafiskrep.grafiskrep.model.Customer;
 import dat19v2.projektgrafiskrep.grafiskrep.model.Machine;
+import dat19v2.projektgrafiskrep.grafiskrep.model.service.Service;
+import dat19v2.projektgrafiskrep.grafiskrep.model.service.ServiceContract;
+import dat19v2.projektgrafiskrep.grafiskrep.model.service.ServiceContractOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class OrderController {
     @ModelAttribute( "items" )
-    List<Machine> items() {
-        //List<Machine> machines = machineDAO.selectAll();
-
-        //NO DB KODE, no machines in DB so added them myself
-        MachineDAO machineDAO = new MachineDAO();
-        List<Machine> machines = new ArrayList<>();
-
-        machines.add(new Machine("Xeox","SejBrand", "x1x1x1x1","Flot printer"));
-        machines.add(new Machine("Etmaerke","FlotBrand", "x2x2x2x2","Flot Bogmaskine"));
-        machines.add(new Machine("Andetmaerke","TramperBrand", "x3x3x3x3","Flot Maskine"));
-        machines.add(new Machine("Mikaelsmor","DunnoBrand", "x4x4x4x4","Flot Mikaels mor"));
-
-        return machines;
+    ArrayList<Machine> items() {
+        return new MachineDAO().selectAll();
     }
 
     @RequestMapping( "/order" )
@@ -35,9 +31,21 @@ public class OrderController {
 
     @PostMapping( "/order" )
     // NOTE: maybe turn it into an Order object?
-    public void sendOrder( String radioChoice, String machine, String firstName, String lastName, String cvr,
+    public void sendOrder( String radioChoice, String machine, String firstName, String lastName, String phoneNr, String cvr,
                                   String email, String address, String postNr, String comment) {
-        System.out.format( "%s, %s, %s, %s, %s, %s, %s, %s, %s\n", radioChoice, machine, firstName, lastName,
+        Customer customer = new Customer( cvr, ( firstName + lastName ), address, phoneNr, email );
+        // NOTE: start and end date are set to null because GrafiskRep will decide when it will start and end with
+        // the customer
+        ServiceContract serviceContract = new ServiceContract( null, customer, null, null );
+        ServiceContractOrder SCO = new ServiceContractOrder( serviceContract, LocalDateTime.now() );
+
+        // TODO: ASK ABOUT DATABASE DESIGN, does idService and idMachineParts need to be primary key in the bridge
+        // TODO: table serviceContracts_has_services?
+
+        new CustomerDAO().insert( customer );
+        new ServiceContractDAO().insert( serviceContract );
+        new ServiceContractOrderDAO().insert( SCO );
+        System.out.format( "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n", radioChoice, machine, firstName, lastName, phoneNr,
                                                                cvr, email, address, postNr, comment );
     }
 }
