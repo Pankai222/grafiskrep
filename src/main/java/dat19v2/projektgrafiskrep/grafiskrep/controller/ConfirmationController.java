e31629410ba8b9271d103c522228b840ceb8a4e7
Niklas Lund Brock <niklasbrock@gmail.com>
2019-12-14 21:42:49 +0100

    Spelling error in footer Refactored Catalogue with th:each

--- src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/ConfirmationController.java
+++ src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/ConfirmationController.java
@@ -1,12 +1,18 @@
 package dat19v2.projektgrafiskrep.grafiskrep.controller;
 
+import dat19v2.projektgrafiskrep.grafiskrep.model.pos.Sale;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ConfirmationController {
     @RequestMapping("/confirmation")
-    public String confirmation(){
+    public String confirmation(HttpSession httpSession, Model model){
+        Sale sale = (Sale) httpSession.getAttribute("sale");
+        model.addAttribute("sale", sale);
         return "confirmation";
     }
 }
