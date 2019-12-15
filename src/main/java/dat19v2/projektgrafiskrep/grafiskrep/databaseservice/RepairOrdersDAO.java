69658c0f835f2e4ede324a7adccae9827b7412e3
Primdahl26 <frederik.vincent@hotmail.com>
2019-12-15 20:45:56 +0100

    Tilføjet RepairOrdersDAO
    Tilføjet selectall i RepairTypeDAO
    
    Tilføjet repairType i repair_order.html
    
    tilføjet cusotmer i Repair.java

---
+++ src/main/java/dat19v2/projektgrafiskrep/grafiskrep/databaseservice/RepairOrdersDAO.java
@@ -0,0 +1,8 @@
+package dat19v2.projektgrafiskrep.grafiskrep.databaseservice;
+
+public class RepairOrdersDAO {
+
+    public void insert(){
+
+    }
+}
--- src/main/java/dat19v2/projektgrafiskrep/grafiskrep/model/pos/Repair.java
+++ src/main/java/dat19v2/projektgrafiskrep/grafiskrep/model/pos/Repair.java
@@ -1,7 +1,10 @@
 package dat19v2.projektgrafiskrep.grafiskrep.model.pos;
 
+import dat19v2.projektgrafiskrep.grafiskrep.model.Customer;
 import dat19v2.projektgrafiskrep.grafiskrep.model.Machine;
 import dat19v2.projektgrafiskrep.grafiskrep.model.MachinePart;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,14 +13,18 @@ public class Repair {
     private RepairType type;
     private Machine machine;
     private ArrayList<MachinePart> parts;
+    private Customer customer;
+    private LocalDateTime date;
 
     public Repair() {
     }
 
-    public Repair(int price, RepairType type, Machine machine, ArrayList<MachinePart> parts) {
+    public Repair(int price, RepairType type, Machine machine, ArrayList<MachinePart> parts, Customer customer, LocalDateTime date) {
         this.type = type;
         this.machine = machine;
         this.parts = parts;
+        this.customer = customer;
+        this.date = date;
         this.price = calcPrice();
     }
 
