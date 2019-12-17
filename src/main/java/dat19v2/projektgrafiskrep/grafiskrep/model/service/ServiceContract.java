87cc3bb09e6b52010bd459527c6e8619fd3ac1e9
Niklas Lund Brock <niklasbrock@gmail.com>
2019-12-17 19:19:18 +0100

    Comments. Changed portfolio images to our own images.

--- src/main/java/dat19v2/projektgrafiskrep/grafiskrep/model/service/ServiceContract.java
+++ src/main/java/dat19v2/projektgrafiskrep/grafiskrep/model/service/ServiceContract.java
@@ -5,6 +5,9 @@ import dat19v2.projektgrafiskrep.grafiskrep.model.Customer;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+//This class is for use with the Database.
+//When an order is approved, it is given a start and end date by the system admin,
+// with the Manage Data Use Case, which has not yet been developed.
 
 public class ServiceContract {
     private ServiceContractOrder SCO;
