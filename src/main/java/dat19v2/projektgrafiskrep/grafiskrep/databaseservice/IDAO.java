792edb4c0feaf816d2442845b2f6a1b372327aaa
mikaelpasovski <mikaelpasovski@gmail.com>
2019-12-18 22:18:56 +0100

    commit to pull

--- src/main/java/dat19v2/projektgrafiskrep/grafiskrep/databaseservice/IDAO.java
+++ src/main/java/dat19v2/projektgrafiskrep/grafiskrep/databaseservice/IDAO.java
@@ -2,7 +2,7 @@ package dat19v2.projektgrafiskrep.grafiskrep.databaseservice;
 
 import java.util.List;
 
-public interface IDAO {
+ public interface IDAO {
     List selectAll();
     void delete();
     void insert();
