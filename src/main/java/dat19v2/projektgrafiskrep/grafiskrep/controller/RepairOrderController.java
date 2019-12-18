252984357cd57038d2373b2dab97709144825d15
Primdahl26 <frederik.vincent@hotmail.com>
2019-12-18 01:30:19 +0100

    Made service_order.html more pretty Made service_amount.html
    Started commenting code

--- src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/RepairOrderController.java
+++ src/main/java/dat19v2/projektgrafiskrep/grafiskrep/controller/RepairOrderController.java
@@ -22,48 +22,36 @@ public class RepairOrderController {
         return "repair_order";
     }
 
+    //Adds the model Machines, which returns an arraylist of machines from database, for use with thymeleaf.
     @ModelAttribute( "items" )
     ArrayList<Machine> items() {
         return new MachineDAO().selectAll();
     }
 
+    //Adds the model repairtypes, which returns an arraylist of repairtypes from database, for use with thymeleaf.
     @ModelAttribute( "repairTypes" )
     ArrayList<RepairType> repairTypes() {
         return new RepairTypeDAO().selectAll();
     }
 
+
     @PostMapping( "submit-order" )
     public String sendOrder(Model model, String repairDate, int repairtypeIndex, int machineIndex, String firstName, String lastName, String phoneNr, String cvr,
                             String email, String address, String postNr, String comment) {
-
 
         Customer customer = new Customer(cvr, firstName+" "+lastName, address, phoneNr, email);
 
         RepairType repairType = repairTypes().get(repairtypeIndex);
 
         Machine machine = items().get(machineIndex);
-        System.out.println(items().get(machineIndex));
         Repair repair = new Repair(repairType, machine, customer, LocalDateTime.parse(repairDate));
+
 
         RepairOrdersDAO repairOrdersDAO = new RepairOrdersDAO();
 
+        //Running the method from our DAO, so that
         repairOrdersDAO.insert(repair);
-
-        System.out.println();
 
         return "/repair_order";
     }
-        /* Commenting out since we want to create a repair
-        Customer customer = new Customer( cvr, ( firstName + lastName ), address, phoneNr, email );
-        // NOTE: start and end date are set to null because GrafiskRep will decide when it will start and end with
-        // the customer
-        ArrayList<Service> services = new ArrayList<>();
-        ServiceContractOrder SCO = new ServiceContractOrder(LocalDateTime.now(),
-                services, customer );
-
-        new CustomerDAO().insert( customer );
-        new ServiceContractOrderDAO().insert( SCO );
-        System.out.format( "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n", repairDate, machine, firstName, lastName, phoneNr,
-                cvr, email, address, postNr, comment );
-    } */
 }
